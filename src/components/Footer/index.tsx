452d38e7226ff89e17a952b6672e57751c956a66
prisha-11 <prisha.bankar24@vit.edu>
2025-12-15 18:14:39 +0530

    Remove contact and newsletter section

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -27,8 +27,9 @@ const Footer = () => {
                   />
                 </Link>
                 <p className="mb-9 text-base leading-relaxed text-body-color dark:text-body-color-dark">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer lobortis.
+                 © 2025 CETify.in  
+                Built for MHT-CET coaching institutes across Maharashtra.
+
                 </p>
                 <div className="flex items-center">
                   <a
